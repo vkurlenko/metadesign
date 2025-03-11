34b67dd3208efb6bd4e22acff0a71d2c76818dfd
Egor Kurlenko <ekurlenko17@gmail.com>
2025-03-11 17:33:04 +0300

    validation

--- src/Controller/Api/CalculateController.php
+++ src/Controller/Api/CalculateController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Order;
+use App\Entity\RealtyType;
+use App\Entity\RepairType;
+use App\Entity\RoomType;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CalculateController extends AbstractController
 {
@@ -45,11 +52,11 @@ class CalculateController extends AbstractController
      * @return Response
      */
     #[Route('/api/calculate', name: 'calculate')]
-    public function index(Request $request): Response
+    public function index(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager): Response
     {
         $data = $request->getPayload()->all();
 
-        if ($this->isValid($data)) {
+        if ($this->isValid($data, $validator, $entityManager)) {
             $result = $this->calculate($data);
 
             $message = $request->getPayload()->get('realty-type') == self::REALTY_TYPE_FLAT
@@ -82,13 +89,59 @@ class CalculateController extends AbstractController
      * @param $data
      * @return bool
      */
-    private function isValid($data): bool
+    private function isValid($data, ValidatorInterface $validator, EntityManagerInterface $entityManager): bool
     {
-        return true;
+        $roomType = new RoomType();
+        $roomType->setName($data['room-type']);
+        $errors = $validator->validate($roomType);
+        if ($errors->count() > 0) {
+            echo 1;
+            return false;
+        }
+
+        $repairType = new RepairType();
+        $repairType->setName($data['repair-type']);
+        $errors = $validator->validate($repairType);
+        if ($errors->count() > 0) {
+            echo 1;
+            return false;
+        }
+
+        $realtyType = new RealtyType();
+        $realtyType->setName($data['realty-type']);
+        $errors = $validator->validate($realtyType);
+        if ($errors->count() > 0) {
+            echo 1;
+            return false;
+        }
+
+        $user = new User();
+        $user->setPhoneNumber(preg_replace('/[^0-9]/', '', $data['phone']));
+        $errors = $validator->validate($user);
+        if ($errors->count() > 0) {
+            return false;
+        }
+
+        $squareArea = $data['area-square'];
+
+        $order = new Order();
+        $order->setRoomType($roomType);
+        $order->setRepairClass($repairType);
+        $order->setPropertyType($realtyType);
+        $order->setUserId($user);
+        $order->setSquare($squareArea);
+        $order->setCreatedAt(new \DateTime('now'));
+        $errors = $validator->validate($order);
+        if ($errors->count() > 0) {
+            return false;
+        }
+
+        return True;
     }
 
     private function calculate(array $data): float
     {
+        echo 0;
         return 0;
     }
 
