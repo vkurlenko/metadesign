263e37a863689d42024fe9e2b4d100733ccf3f35
Egor Kurlenko <ekurlenko17@gmail.com>
2025-04-06 22:24:37 +0300

    commit

--- public/js/main.js
+++ public/js/main.js
@@ -25,7 +25,7 @@ $(window).on('load', function() {
 
 
 (function($) {
-	$( "#accordion h3" ).on('click', function (e) {
+	$( "#accordion > div:not(:first-child) h3" ).on('click', function (e) {
 		$(this).next('.service-item-wrapper').eq(0).toggle();
 		$(this).find('.toggle-down').eq(0).toggle();
 		$(this).find('.toggle-up').eq(0).toggle();
