c0f2ab5ddd21d2113c087691d9dc04e054b81e71
bublik911 <ekurlenko17@gmail.com>
2025-08-10 19:05:58 +0300

    $repair_class -> $repairType

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -18,7 +18,7 @@ class OrderService
 
         private RealtyTypeRepository $realtyTypeRepository,
         private RepairTypeRepository $repairTypeRepository,
-        private RealtyStatusTypeRepository $roomTypeRepository,
+        private RealtyStatusTypeRepository $realtyStatusTypeRepository,
         private OrderRepository $orderRepository,
     ) {
     }
@@ -31,14 +31,14 @@ class OrderService
 
             $realtyType = $this->realtyTypeRepository->findOneByName($data->realtyType);
             $repairType = $this->repairTypeRepository->findOneByName($data->repairType);
-            $roomType = $this->roomTypeRepository->findOneByName($data->roomType);
+            $realtyStatusType = $this->realtyStatusTypeRepository->findOneByName($data->realtyStatusType);
             $squareArea = $data->areaSquare;
             $user = $this->userService->findOrCreateByPhone($phone);
 
             $order = new Order();
-            $order->setPropertyType($realtyType);
-            $order->setRepairClass($repairType);
-            $order->setRoomType($roomType);
+            $order->setRealtyType($realtyType);
+            $order->setRepairType($repairType);
+            $order->setRealtyStatusType($realtyStatusType);
             $order->setSquare($squareArea);
             $order->setUserId($user);
             $order->setCreatedAt(new \DateTime('now'));
