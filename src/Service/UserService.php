b6be4ec8e224d3d91e25c9e833397207f5a12abf
bublik911 <ekurlenko17@gmail.com>
2025-08-10 12:08:05 +0300

    UserService

---
+++ src/Service/UserService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+class UserService
+{
+    public function __construct(
+        private UserRepository $userRepository
+    ) {
+    }
+
+    public function findOrCreateByPhone(string $phone, ?string $name = null): User
+    {
+        $user = $this->userRepository->findOneByPhoneNumber($phone);
+
+        if ($user) {
+            return $user;
+        }
+
+        $user = new User();
+        $user->setPhoneNumber($phone);
+        $user->setFirstName($name);
+        $this->userRepository->save($user);
+
+        return $user;
+    }
+}
